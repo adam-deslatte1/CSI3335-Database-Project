6f4890883ee9e9e2473b394f163feb570cf1db54
Eugene Pak <eugene@koyoshiga.local>
2025-05-05 23:21:52 -0500

    Simple fix

--- routes/main.py
+++ routes/main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request, flash
 from flask_login import login_required, current_user
-from models import db, User, UserSelectionLog, NoHitter
+from models import db, User, NoHitter
 
 main = Blueprint('main', __name__)
 
